4fff67180657e998065d2f96420c794678666be9
Mason Wesolek <masonwesolek@Masons-MacBook-Pro.local>
2016-12-27 01:11:59 -0500

    deleted old files  Please enter the commit message for your changes. Lines starting

--- FoodTracker/CalendarViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CalendarViewController.swift
-//  FoodTracker
-//
-//  Created by Mason Wesolek on 11/15/16.
-//  Copyright © 2016 Mason Wesolek. All rights reserved.
-//
-
-import Foundation
